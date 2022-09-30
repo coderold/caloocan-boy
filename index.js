266e53b4fdf429628d7bbb49c8fbfa3456cae541
coderold <mm35molina@gmail.com>
2022-09-30 19:38:46 +0800

    sana gumana

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const btn = document.getElementById("enter");
 const home = document.getElementById("home");
 const pic = document.getElementById("picture");
+const tite = document.getElementById("tite");
 var audio = new Audio("/files/music.mp3");
 
 btn.onclick = function () {
     home.style.display = "none";
     pic.style.display = "block";
     audio.play();
+}
+
+tite.onclick = function (){
+    dis.style.display = "block";
 }
